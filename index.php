8cb9c24b9145b0347f8ac10a21566516335e0e5a
Hayden Cederstrom <hayden.cederstrom@dynatrace.com>
2019-06-17 15:31:44 +0000

    added composer.json file

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
-    
-    echo  "This is my first PHP file!";
-    
-?>
+<html>
+    <head>
+        <title>This is my PHP Website</title>
+    </head>
+</html>
+<body>
+<center>
+    <h1>
+        <?php
+        
+        // echo out some text
+        echo  "Page Title!";
+        
+        $variable_one = "This is my first variable";
+        $number_variable = 41;
+
+        
+        ?>
+    </h1>
+</center>
+
+</body>
